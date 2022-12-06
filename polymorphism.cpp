d40a3daeb56912e2f924f5cf96e6f6da1f74b78e
Darius Ghomashchian <50425882+Darius0852@users.noreply.github.com>
2022-12-06 23:06:59 +0000

    Nothing

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
-
 using namespace std;
-
 class Addition {
-    public:
-    int ADD(int X, int Y) //Function with parameter
+public:
+    int ADD(int X,int Y)   // Function with parameter 
     {
-        return X + Y; //this function is performing addition of two integers
-    }
-    int ADD() {                 //Function with the same name but with parameter
-        string a = "Hello";
-        string b="Darius";      //concatenation is performed
-        string c = a + b;
-        cout << c<< endl;
-    }
+        return X+Y;     // this function is performing addition of  two Integer value
+    };
+    int ADD() {              // Function with same name but without parameter
+        string a= "HELLO";
+        string b="SAM";   // in this function concatenation is performed
+       string c= a+b;
+       cout<<c<<endl;
+       return 0;
+    };
 };
-
 int main(void) {
-    Addition obj;   //Object is created
-    cout <<obj.ADD(128, 15)<<endl; //first methos is called
-    obj.ADD(); //second methos is called
+    Addition obj;   // Object is created  
+    cout<<obj.ADD(128, 15)<<endl; //first method is called
+    obj.ADD();  // second method is called
     return 0;
-}
+};
